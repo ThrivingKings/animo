ebba221b0d4af0d149c88a2e0d3e5e9e9e64fe1a
ThrivingKings <danielraftery00@gmail.com>
2016-06-25 11:54:51 -0400

    adds countdown package

---
+++ packages/countdown/src/index.js
@@ -0,0 +1,42 @@
+import animo from 'animo-core'
+
+const countdown = (el, options) => {
+  const defaultProps = {
+    color: '#ddd',
+    elapsed: 0,
+    total: 60
+  }
+
+  const state = {
+    elapsed: 0,
+    progressEl: document.createElement('div')
+  }
+
+  const props = { ...defaultProps, ...options }
+  const { elapsed, total } = props
+  state.elapsed = elapsed
+  const iterate = total - elapsed
+
+  const percent = elapsed =>
+    Math.max(0, 100 - ((elapsed / total) * 100 ))
+
+  return new animo(el, {
+    iterate,
+    onMount: element => {
+      element.raw.appendChild(state.progressEl)
+      Object.assign(state.progressEl.style, {
+        backgroundColor: props.color,
+        height: '100%'
+      })
+
+      element.transform(`translate3d(${percent(state.elapsed)}%, 0, 0)`)
+    },
+    onIteration: element => {
+      state.elapsed++
+      element.transform(`translate3d(${percent(state.elapsed)}%, 0, 0)`)
+      element.transition('transform 1s linear')
+    }
+  })
+}
+
+module.exports = countdown
